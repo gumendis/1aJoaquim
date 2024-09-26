e5637b8ca61d6183a3d2bff6ffffc4e0eb7fef19
gumendis <gustavo.mendes08@escola.pr.gov.br>
2024-09-26 12:04:42 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// as aventuras de pi, 10, drama, fantasia, aventura
+
+// Ladrões de bicicleta, 12, drama.
+// depois da chuva, 10, drama
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+
+  let campoIdade;
+  let campoFantasia;
+
+function setup() {
+    createCanvas(800, 400);
+    campoIdade = createInput("5");
+    campoFantasia = createCheckbox("Gosta de fantasia?");
+}
+
+function draw() {
+    background(220);
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+
+    textAlign(CENTER, CENTER);
+    textSize(38);
+    text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia) {
+    if(idade >= 10) {
+        if(idade >= 14) {
+            return "O menino que descobriu o vento";
+        } else {
+            if(gostaDeFantasia){
+                return "As aventuras de Pi";
+            } else {
+                return "Depois da chuva";
+            }
+        }
+    } else {
+        if(gostaDeFantasia) {
+            return "A viagem de Chihiro";
+        } else {
+            return "O feitiço do tempo";
+        }
+    }
+}
